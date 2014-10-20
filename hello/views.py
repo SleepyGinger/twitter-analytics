6029050948f0c5663b4c4b0e4006daa9f066c277
Dan <Salazar@nyu.edu>
2014-10-19 21:43:27 -0700

    testing graph functionality

--- hello/views.py
+++ hello/views.py
@@ -9,6 +9,11 @@ import time
 import re
 import pandas as pd
 import collections
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 import requests
 
@@ -22,6 +27,19 @@ def index(request):
 	html_output += '</body></html>'
 	return HttpResponse(html_output)
 
+def day_graph(source):   
+    date_by_years=pd.to_datetime(source, coerce=True).values.astype('datetime64[D]')
+    pd.Series(date_by_years).value_counts().plot(marker='o', linestyle='-')
+    plt.xticks(rotation=30)
+    plt.show()
+    #return(matplotlib.pyplot.savefig('d.png'))
+
+def year_graph(source):   
+    date_by_years=pd.to_datetime(source, coerce=True).values.astype('datetime64[Y]')
+    pd.Series(date_by_years).value_counts().plot(marker='o', linestyle='-')
+    plt.xticks(rotation=30)
+    plt.show()
+    #return(matplotlib.pyplot.savefig('y.png'))
 
 def analyze(request):
 	screen_name = request.GET["handle"].replace('@','')
@@ -81,6 +99,7 @@ def analyze(request):
 	################################################################################
 	dd = pd.DataFrame(outtweets)
 	dd.columns = ['RT_count', 'reply_id', 'Tweet', 'at_message_id', 'date', 'at_message_user', 'favorited_count' ]
+	year_graph(dd['date'])
 	btop_messaged_user=str(dd['at_message_user'].value_counts()[:5])
 	top_messaged_user=btop_messaged_user.replace("\n", "'<br>'").replace("dtype: int64","").replace("'","")
 
@@ -161,7 +180,7 @@ def analyze(request):
 	num_RT = interesting_df["Times_RTd"].sum()
 
 	
-	html_output=  '<html><head><title>Twitter Analyzer</title></head><body style="background-image: url('+background+'); background-position: right top; background-repeat: no-repeat; background-attachment: fixed; background-position: 95% 2%;  background-size: 250px 90px; ">'
+	html_output=  '<html><head><title>Twitter Analyzer</title></head><body style="background-image: url('+background+'); background-position: right top; background-repeat: no-repeat; background-attachment: fixed; background-position: 3% 2%;  background-size: 250px 90px; ">'
 	html_output+= '<center><font size="11"><img src='+profile_img+' alt=Profile_Pic>'
 	html_output+= ' '+ name+ "'s Twitter Overview </font>"
 	html_output+= '<br>'
@@ -182,7 +201,9 @@ def analyze(request):
 	html_output+= '<br>' + PtweetsRTd +" (" + str(tweetsRTd)+ " tweets) of @" +screen_name+ "'s tweets were retweeted " + str(num_RT) + " times"
 	html_output+= '<br>' + "@" +screen_name+ " received " + str(totalfavorited)+  " favorites"
 	html_output+= '<br><br>Top 5 direct messaged users with frequencies<br>'
-	html_output+= top_messaged_user+'</center>'
+	html_output+= top_messaged_user
+	html_output+= '<br> <img src=/tmp/y.png>'
+	html_output+='</center>'
 	#html_output+= 'Top Favorited tweets'
 	#html_output+= fav
 
